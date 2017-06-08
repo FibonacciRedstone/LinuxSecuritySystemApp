70e9bdf40d2e5a48155b61f4dd40fd5117ff73b2
FibonacciRedstone <bcarlson336@gmail.com>
2017-06-08 09:36:40 -0600

    Initial Commit

---
+++ Security AccessTests/Security_AccessTests.swift
@@ -0,0 +1,36 @@
+//
+//  Security_AccessTests.swift
+//  Security AccessTests
+//
+//  Created by Owner on 6/8/17.
+//  Copyright © 2017 FibonacciCorp. All rights reserved.
+//
+
+import XCTest
+@testable import Security_Access
+
+class Security_AccessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
